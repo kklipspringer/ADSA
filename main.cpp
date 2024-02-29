c81baceaee8bf24dc6aaf4901b60b797c39b37b3
kklipspringer <mennobrandt@protonmail.com>
2024-02-29 16:46:32 +1030

    Doesnt work seg fault

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 // Please note, that I've replaced I1 with 'a', and I2 with 'b',
 
-
+// Reads and formats input into vector forms
 std::vector<std::vector<int>> getInput() {
     // Read inputs 'a' and 'b' as strings
     // Read input B (Base) as an int
@@ -32,8 +32,8 @@ std::vector<std::vector<int>> getInput() {
     return result;
 }
 
+// Add leading zeroes, if digits(a) != digits(b)
 void addZeroes(std::vector<int>& a, std::vector<int>& b) {
-    // Add leading zeroes, if digits(a) != digits(b)
     int lenA = a.size(), lenB = b.size();
     int deltaDigits = lenA - lenB;
     if(lenA > lenB) { 
@@ -45,6 +45,21 @@ void addZeroes(std::vector<int>& a, std::vector<int>& b) {
     }
 }
 
+// Split a vector, into two equal parts (for Karatsuba multiplication)
+std::vector<std::vector<int>> splitVec(const std::vector<int>& a) {
+    size_t half = a.size() / 2;
+    std::vector<int> left(a.begin(), a.begin() + half); // high
+    std::vector<int> right(a.begin() + half, a.end()); // lower
+
+    std::vector<std::vector<int>> splitParts;
+    splitParts.push_back(left);
+    splitParts.push_back(right);
+
+    return splitParts;
+}
+
+
+// Addition (vector form) with different bases
 std::vector<int> schoolAddition(std::vector<int> a, std::vector<int> b, int base) {
     std::vector<int> s; // final sum
     int carry = 0;
@@ -64,9 +79,38 @@ std::vector<int> schoolAddition(std::vector<int> a, std::vector<int> b, int base
     return s;
 }
 
-std::vector<int> karatsubaMult(std::vector<int> a, std::vector<int> b, int base) {
-    
+// Subtraction (vector form) with different bases
+std::vector<int> schoolSubtraction(std::vector<int> a, std::vector<int> b, int base) {
+    std::vector<int> res;
+    int borrow = 0;
+
+    for(int i = (a.size() - 1); i >= 0; i++) {
+        int sub = a[i] - b[i] - borrow;
+        
+        // carry over if subtraction negative
+        if(sub < 0) {
+            sub += base;  
+            borrow = 1; 
+        } else {
+            borrow = 0; // reset borrow
+        }
+
+        res.insert(res.begin(), sub);
+    }
+
+    // Take away zeroes at front
+    while((res.size() > 1) && (res[0] == 0)) {
+        res.erase(res.begin());
+    }
+
+    return res;
 }
+
+// Karatsuba Multiplication. 
+std::vector<int> karatsubaMult(std::vector<int> a, std::vector<int> b, int base) {
+
+}
+
 
 int main() {
     // Declaration of final results 
@@ -75,27 +119,27 @@ int main() {
     // Ask for input, and store in their own vectors: 
     // Access order: [0: vecA, 1:vecB, 2: Base] 
     std::vector<std::vector<int>> inputs = getInput();
-    // Seperate into 2 vectors. Seperate base into an int
+    // Separate into 2 vectors. Separate base into an int
     std::vector<int> a = inputs[0], b = inputs[1];
     addZeroes(a, b); // add zeroes, if numbers are different lengths
     int base = inputs[2][0]; 
 
     // School Addition: 
     schoolRes = schoolAddition(a, b, base);
-    int schoolSize = schoolRes.size();
+    size_t schoolSize = schoolRes.size();
 
     // Karatsuba Multiplication:
     karatRes = karatsubaMult(a, b, base);
-    int karatSize = karatRes.size();
+    size_t karatSize = karatRes.size();
 
     // Format the outputs. Addition_Multiplication_0 (no division)
-    for(int i = 0; i < schoolSize; i++) { 
+    for(size_t i = 0; i < schoolSize; i++) { 
         std::cout << schoolRes[i];
     }
 
     std::cout << " ";
 
-    for(int i = 0; i < karatSize; i++) {
+    for(size_t i = 0; i < karatSize; i++) {
         std::cout << karatRes[i];
     }
 
