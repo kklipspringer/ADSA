84c683cc940aff13432b425d73587cc942b8d1a8
kklipspringer <mennobrandt@protonmail.com>
2024-04-13 23:12:59 +0930

    skeleton

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+struct Node {
+    int key;
+    int height;
+    Node* left;
+    Node* right;
+};
+
+class AVL {
+    public:
+        // Balancing
+
+        // Insertion
+
+        // Print out trees
+        void preOrder() {
+
+        }
+
+        void inOrder() {
+
+        }
+
+        void postOrder() {
+
+        }
+
+        // Deletion
+};
+
+int main(void) {
+    // Get input from user
+
+}
