b61013cafc7ab4a29ba08a53d14cb83e5df581c0
kklipspringer <mennobrandt@protonmail.com>
2024-05-02 23:42:06 +0930

    Finished Search and Insert functions

--- main.cpp
+++ main.cpp
@@ -23,6 +23,9 @@ using namespace std;
 // vector<string> getInput() {
 
 // }
+vector<string> getInput() {
+    // tokenise and get input
+}
 
 struct Entry {
     string key; // Single letter
@@ -46,26 +49,58 @@ class hashTable {
 
         // Take word in. Use last letter. Convert letter to hash table index. 
         int hash(string key) {
-            if(key.empty()) {
-                return (-1);
-            }
-
             char lastChar = key.back(); // access last char. 
             return (lastChar - 'a')  % 26; 
         }
 
+        // Start at the hashed entry. Then keep looking, and eventually wrap around. Do this 26 times.
         bool Search(string key) {
             int potentialIndex = hash(key);
-            //.... go to index. is it there? no? increase index. Is it there? no? increase index.
-            // If you've reached the end, return. 
+            int maxIterations = 26 - hash(key);
+
+            for(int i = 0; i < 26; i++) {
+                // To be found, it must be marked occupied, and the keys must match. 
+                if((hTable[potentialIndex].key == key) && (hTable[potentialIndex].status == "Occupied")) {
+                    return true; 
+                } else {
+                    potentialIndex = (potentialIndex + 1) % 26; // to wrap around properly. 
+                }
+            }
+
+            // If it's looked through 26 entries, then it doesn't exist, or it's been deleted.
+            // Therefore, return false.  
+            return false; 
         }
 
-        void Insert() {}
+        void Insert(string key) {
+            // If it exists, return. 
+            if(Search(key)) {
+                return;
+            }
+
+            int targetIndex = hash(key); // position in hash table, where you intend to store the key.  
+            // Similar wrap around technique, to Search() function. 
+            for(int i = 0; i < 26; i++) {
+                if((hTable[targetIndex].status == "Never Used") || (hTable[targetIndex].status == "Tombstone")) {
+                    hTable[targetIndex].key = key; 
+                    hTable[targetIndex].status = "Occupied";
+                    return;
+                } 
+                // If it's occupied, increase the index. Eventually wrap around. 
+                if(hTable[targetIndex].status == "Occupied") {
+                    targetIndex = (targetIndex + 1) % 26;  
+                } 
+            }
+        }
 
         void Delete(string target) {}
 
         void printTable() {}
 };
+
+void executeInstructions(vector<string> instructions) {
+    
+}
 
 int main(void) {
     // Read input
