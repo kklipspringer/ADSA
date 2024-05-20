21379cd122c910747ca0f01fd499203e430bece0
kklipspringer <mennobrandt@protonmail.com>
2024-05-20 18:58:00 +0930

    Ideas. No functionality.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
-// Assignment 4 - Graph Algorithms
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+/* 
+
+Approach Explained:
+    - Read input. The three 2D arrays (which I'll be handling as vectors)
+    - Convert the build, and destroy 2D vectors to numerical costs. 
+*/
+
+
+
+
+
+// 
+vector<vector<int>> costConversion(vector<vector<string>> *) {
+
+}
+
+
+
+int main(void) {
+
+}
