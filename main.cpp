915bcfdcbee95b777913653e72187b7b9a1fa797
kklipspringer <mennobrandt@protonmail.com>
2024-05-21 23:10:43 +0930

    Function signatures and some structure.

--- main.cpp
+++ main.cpp
@@ -14,14 +14,32 @@ vector<string> split(const string& s, char delimiter) {
     return tokens;
 }
 
+// Return 2D vectors. Do necessary conversion from letters to numbers. 
+    /*
+                   _______
+        abc,def    |a b c|
+                   |d e f|
+            
+    */
+vector<vector<int>> stringTo2DVector(string splitLine) {
+    
+}
 
+vector<vector<int>> makeGraph(vector<vector<int>> towns, vector<vector<int>> build, vector<vector<int>> destroy) {
+
+}
 
 int main() {
-    // Get input. First split by spaces. 
+    // Parse Input
+        // Example 1: 000,000,000 ABD,BAC,DCA ABD,BAC,DCA
+        // Example 2: 011,101,110 ABD,BAC,DCA ABD,BAC,DCA
     string input;
     getline(cin, input); 
     vector<string> parts = split(input, ' ');
 
-    // Then examine each character and add as entries. 
-    return 0;
+    vector<vector<int>> towns = stringTo2DVector(parts[0]);
+    vector<vector<int>> build = stringTo2DVector(parts[1]);
+    vector<vector<int>> destroy = stringTo2DVector(parts[2]);
+
+    // Construct Graph. Basically if towns[i][j] exists, consider build or destroy costs. 
 }
