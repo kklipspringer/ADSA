62c2a7147b7f7ab498194254b7bf2f90cdb5a703
kklipspringer <mennobrandt@protonmail.com>
2024-05-23 10:01:52 +0930

    Convert input to 2D vectors, and construct edge list. Doesnt consider lowercase letters.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <sstream>
 
+// country, build, destroy
+
 using namespace std;
 
 vector<string> split(const string& s, char delimiter) {
     vector<string> tokens;
-    string token;
+    string token; 
     istringstream tokenStream(s);
     while (getline(tokenStream, token, delimiter)) {
         tokens.push_back(token);
@@ -19,14 +21,46 @@ vector<string> split(const string& s, char delimiter) {
                    _______
         abc,def    |a b c|
                    |d e f|
-            
     */
 vector<vector<int>> stringTo2DVector(string splitLine) {
-    
+    vector<vector<int>> intVector;
+    vector<string> rows = split(splitLine, ',');
+    for(string row : rows) {
+        vector<int> intRow;
+        for(char c : row) {
+            if(isdigit(c)) {
+                intRow.push_back(c - '0'); // character to integer
+            } 
+            
+            if(isalpha(c)) {
+                intRow.push_back(c - 'A'); // letter to number
+            } 
+        }
+        intVector.push_back(intRow);
+    }
+    return intVector;
 }
 
-vector<vector<int>> makeGraph(vector<vector<int>> towns, vector<vector<int>> build, vector<vector<int>> destroy) {
+vector<vector<int>> makeGraph(vector<vector<int>> country, vector<vector<int>> build, vector<vector<int>> destroy) {
+    // Connection at Country[x][y]?
+        // Yes: What is the destroy cost? 
+        // No: What is the build cost? 
 
+    // Doing this to initialise the final graph, and I'm considering build to be the default state. 
+    vector<vector<int>> finalGraph = build; 
+    
+    for(size_t i = 0; i < country.size(); i++) {
+        for(size_t j = 0; j < country[i].size(); j++) {
+            if(country[i][j] == 1) {
+                finalGraph[i][j] = (destroy[i][j] * -1);
+            } 
+            // if(country[i][j] == 0) {
+            //     finalGraph[i][j] = build[i][j];
+            // } 
+        }
+    }
+
+    return finalGraph;
 }
 
 int main() {
@@ -37,9 +71,20 @@ int main() {
     getline(cin, input); 
     vector<string> parts = split(input, ' ');
 
-    vector<vector<int>> towns = stringTo2DVector(parts[0]);
+    vector<vector<int>> country = stringTo2DVector(parts[0]);
     vector<vector<int>> build = stringTo2DVector(parts[1]);
     vector<vector<int>> destroy = stringTo2DVector(parts[2]);
 
-    // Construct Graph. Basically if towns[i][j] exists, consider build or destroy costs. 
+    // Construct Graph. 
+    vector<vector<int>> graph = makeGraph(country, build, destroy);
+
+    for(size_t i = 0; i < graph.size(); i++) {
+        for(size_t j = 0; j < graph[i].size(); j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl; 
+    }
+
+    // Perform Kruskal's on Graph.
+    return 0; 
 }
